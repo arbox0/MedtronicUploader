3d6fc2d9e486fd17be83536a869b8261fe930fc2
David <koyli@users.noreply.github.com>
2017-10-25 20:44:38 +0100

    Remove dexcomG4 support; remove unused classes

--- src/com/nightscout/android/upload/GlucometerRecord.java
+++ src/com/nightscout/android/upload/GlucometerRecord.java
@@ -6,14 +6,6 @@ public class GlucometerRecord  extends Record implements Serializable{
     public float numGlucometerValue = 0;
     public long lastDate = 0;
     
-    private static final long serialVersionUID = 4654897648L;	
-    
-    public void setNumGlucometerValue(int numGlucometerValue) {
-		this.numGlucometerValue = numGlucometerValue;
-	}
-
-	public void setLastDate(long lastDate) {
-		this.lastDate = lastDate;
-	}
+    private static final long serialVersionUID = 4654897648L;
 
 }
